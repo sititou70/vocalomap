69b6240715b087e3fafd655992be4d2a177bf1b0
Takuya Shizukuishi <sititou70@gmail.com>
2020-04-09 22:26:00 +0900

    Updates

---
+++ precache-manifest.de620b9bbfba235215bf8af690a62e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d5fa3d0bbfe40c6d4519d99a963abd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b035632d3761c0b7914a",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b035632d3761c0b7914a",
+    "url": "./static/js/2.f9193d10.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "./static/js/2.f9193d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e047e116039236ed286d",
+    "url": "./static/js/main.756361af.chunk.js"
+  },
+  {
+    "revision": "81a0f0bf67eeb947c642",
+    "url": "./static/js/runtime-main.4ca50066.js"
+  }
+]);
